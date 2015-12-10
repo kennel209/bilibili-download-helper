f825f04ec32d750b3f15d7a6488ecc262bfa8252
Eric Cai <kennel209@gmail.com>
2015-12-11 00:19:49 +0800

    New automatic script & README

--- README.rst
+++ README.rst
@@ -10,14 +10,15 @@ you-get-downloader-helper
 这是什么？
 ============
 
-只是一个使用 ``you-get`` 下载的包装器。
+you_get_download_bilibili.py
+只是一个使用 ``you-get`` 和 ``aria2`` 下载的包装器。
 
-你需要保证路径中可以使用 ``you-get`` 来使用这个脚本。
+你需要保证路径中可以使用 ``you-get`` 和 ``aria2c`` 来使用这个脚本。
 
 python3.4 -_-b
 
 特性
-============
+------------
 
 * 为了方便使用多线程下载（ aria2 ）预设10段10线程(-x 10 -s 10)
 * 单线程也可使用wget
@@ -25,7 +26,7 @@ python3.4 -_-b
 * 暂时不支持单P多分段下载
 
 TODO
-============
+------------
 
 * 单P多分段下载支持
 * 更多option支持
@@ -33,7 +34,7 @@ TODO
 * 直接you-get集成（PR）
 
 用法
-============
+------------
 
 .. code:: console
 
@@ -49,6 +50,8 @@ TODO
     usage: you_get_download_bilibili.py [-h] [-i RANGE] [-s START] [-o PREFIX]
                                     [-d DOWNLOADER] [-f] [-n] [-v]
                                     baseurl
+
+    A small script to help downloading Bilibily video via you-get & aria2
 
     positional arguments:
       baseurl               bash to generate bilibi urls
@@ -70,7 +73,7 @@ TODO
 
 
 用法例子
-============
+-----------
 
 .. code:: console
     
@@ -86,5 +89,49 @@ TODO
     $ ./you_get_download_bilibili.py http://www.bilibili.com/video/av2149245/ -f -s 2 -o "Animelo Summer LIVE"
 
     http://www.bilibili.com/video/av2149245/index_02.html -> Animelo Summer LIVE.flv
+
+
+另一个傻瓜脚本
+================
+
+you_get_download_bilibili_automatic.py 自动化了获取title和index过程，直接下载
+
+用法
+------------
+
+.. code:: console
+
+    $ ./you_get_download_bilibili_automatic.py URL
+
+    # help
+    $ ./you_get_download_bilibili_automatic.py -h
+
+    usage: you_get_download_bilibili_automatic.py [-h] [-n] [-v] baseurl
+
+    Bilibili One URL automatic Downloader Via you-get & aria2
+
+    positional arguments:
+      baseurl        bash to generate bilibili urls
+
+    optional arguments:
+      -h, --help     show this help message and exit
+      -n, --dry-run  just print info, do not actually downdloading
+      -v, --verbose  more info
+
+
+用法例子
+-----------
+
+.. code:: console
+    
+    $ ./you_get_download_bilibili_automatic.py http://www.bilibili.com/video/av1242782/
+
+    Title: 【Vmoe字幕组】LiSA LiVE is Smile Always in武道馆
+    Parts: 5
+    http://www.bilibili.com/video/av1242782/index_01.html -> 【Vmoe字幕组】LiSA LiVE is Smile Always in武道馆_01.flv
+    http://www.bilibili.com/video/av1242782/index_02.html -> 【Vmoe字幕组】LiSA LiVE is Smile Always in武道馆_02.flv
+    http://www.bilibili.com/video/av1242782/index_03.html -> 【Vmoe字幕组】LiSA LiVE is Smile Always in武道馆_03.flv
+    http://www.bilibili.com/video/av1242782/index_04.html -> 【Vmoe字幕组】LiSA LiVE is Smile Always in武道馆_04.flv
+    http://www.bilibili.com/video/av1242782/index_05.html -> 【Vmoe字幕组】LiSA LiVE is Smile Always in武道馆_05.flv
 
 
---
+++ you_get_download_bilibili_automatic.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+# coding: utf8
+
+import sys
+import os
+from urllib.request import urlopen
+from urllib.error import URLError
+from html import unescape
+import argparse
+import gzip
+import re
+import you_get_download_bilibili
+
+DEBUG=False
+
+def debug(s,out=sys.stdout):
+    '''common DEBUG function, depend on Glogal DEBUG'''
+    if DEBUG:
+        print("DEBUG: {!s}".format(s),file=out)
+
+def set_debug(flag):
+    '''SET DEBUG flag recursively'''
+    global DEBUG
+    DEBUG = flag
+    you_get_download_bilibili.set_debug(flag)
+
+def get_url(url,encoding='utf-8'):
+    u'''urlopen to getdata'''
+    try:
+        r = urlopen(url)
+        debug((r.geturl(),r.status,r.reason))
+        if r.status == 200:
+            data = r.read()
+            # gzip to decompress
+            comp = r.getheader("Content-Encoding")
+            if "gzip" == comp:
+                data = gzip.decompress(data)
+            # b'string' -> 'string'
+            data_u = data.decode(encoding)
+            #debug(data_u)
+            return data_u
+        else:
+            print(r.status,r.reason,"OPEN URL ERROR")
+            sys.exit(1)
+    except URLError as err:
+        print(err)
+        sys.exit(1)
+
+def extract_info(data):
+    u'''使用re解析html，获取title和index'''
+
+    title = ""
+    # 非贪婪搜索
+    title_pattern = re.compile(r'<h1\s+title="([^"]+?)">')
+    title_match = title_pattern.search(data)
+    if title_match:
+        title = title_match.group(1)
+        # unquote html entities
+        title = unescape(title.strip())
+    debug(title)
+
+    index = 1
+    # 非贪婪搜索
+    text_pattern = re.compile(r'<div id="plist">(.*?)</div>',re.S)
+    text_match = text_pattern.search(data)
+    if text_match:
+        #debug(text_match.groups())
+        index = text_match.group(1).count(r"<option")
+    if index == 0:
+        index = 1
+    debug(index)
+
+    return title,index
+        
+def do_work(args):
+    url = args.baseurl
+    data = get_url(url)
+    title,index = extract_info(data)
+    fixed_prefix = True if index == 1 else False
+    print("Title: {}".format(title))
+    print("Parts: {}".format(index))
+    you_get_download_bilibili.download(url,
+                                        range=index,
+                                        name_prefix=title,
+                                        fixed_prefix=fixed_prefix,
+                                        dry_run=args.dry_run)
+
+def main():
+    u'''解析命令行参数'''
+
+    parser = argparse.ArgumentParser(description=u'''Bilibili One URL automatic Downloader Via you-get & aria2''')
+    parser.add_argument("baseurl", 
+                        help="bash to generate bilibili urls")
+    parser.add_argument("-n","--dry-run", 
+                        action="store_true", 
+                        help="just print info, do not actually downdloading")
+    parser.add_argument("-v","--verbose", 
+                        action="store_true", 
+                        help="more info")
+    
+    args = parser.parse_args()
+
+    # 调试模式全局变量
+    set_debug(args.verbose)
+    debug(args)
+    do_work(args)
+
+    
+if __name__=="__main__":
+    main()
+
